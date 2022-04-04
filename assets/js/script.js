3cd9550db3fd2afd10c14fc462b6317b7d58a55d
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-03 23:47:43 -0400

    Added functions involving local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,32 @@ var cardArray = [day1El, day2El, day3El, day4El, day5El];
 var apiKey = "607439a3ad59adef49501bad43c27015";
 var historyEl = document.querySelector(".history");
 var clearHistoryEl = document.querySelector(".clear-history");
+
+// saving recent search to local storage
+
+var savedCities = JSON.parse(localStorage.getItem("recentCities")) || [];
+
+// saved cities
+
+function saveRecentCities(city) {
+  savedCities.push(city);
+  localStorage.setItem("recentCities", JSON.stringify(savedCities));
+  generateSearchHistory();
+}
+
+// showing recent searches
+
+function generateSearchHistory() {
+  historyEl.innerHTML = "";
+  for (var i = 0; i < savedCities.length; i++) {
+    var recentItem = document.createElement("li");
+    recentItem.setAttribute("class", "col-10-m-2 btn btn-primary");
+    recentItem.textContent = ("cities", savedCities[i]);
+    historyEl.appendChild(recentItem);
+    recentItem.addEventListener("click", function (e) {
+      var city = e.target.textContent;
+      searchCityEl.value = city;
+      getCurrentWeather(city);
+    });
+  }
+}
