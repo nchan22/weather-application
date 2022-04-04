d777d3f014eb02f10b171bfa0daf19a5b40756e8
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-03 23:46:53 -0400

    added global variables to js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var searchCityEl = document.querySelector(".searchCity");
+var searchBtnEl = document.querySelector(".searchBtn");
+var currentWeatherEl = document.querySelector(".currentWeather");
+
+var day1El = document.querySelector(".day-1");
+var day2El = document.querySelector(".day-2");
+var day3El = document.querySelector(".day-3");
+var day4El = document.querySelector(".day-4");
+var day5El = document.querySelector(".day-5");
+
+var cardArray = [day1El, day2El, day3El, day4El, day5El];
+var apiKey = "607439a3ad59adef49501bad43c27015";
+var historyEl = document.querySelector(".history");
+var clearHistoryEl = document.querySelector(".clear-history");
